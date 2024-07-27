ceca323779cbf200e30cf5665db78223cc91d4a4
Dzhanaur1 <3923328@gmail.com>
2024-07-27 14:57:19 +0300

    add Yandex Metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,6 +98,27 @@ export default function RootLayout({
       </head>
       <body>
         <Script
+          id="yandex-metrica"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
+              (m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(97931300, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `,
+          }}
+        />
+        <Script
           src="https://kit.fontawesome.com/0abf972db9.js"
           crossOrigin="anonymous"
           strategy="afterInteractive"
