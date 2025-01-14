39853df9a0e34b5b51016d5f3ae38a796922c4a1
Dzhanaur1 <3923328@gmail.com>
2025-01-14 10:52:40 +0300

    fix

--- src/app/(components)/WhatsAppButton.tsx
+++ src/app/(components)/WhatsAppButton.tsx
@@ -7,10 +7,10 @@ import { FloatingWhatsApp } from "react-floating-whatsapp";
 const WhatsAppButton = () => {
   return (
     <FloatingWhatsApp
-      phoneNumber="+8918392332" // Укажи свой номер телефона
-      accountName="DigitalSteps.Tech" // Название компании
+      phoneNumber="+79936223328" // Укажи свой номер телефона
+      accountName="DigitalSteps" // Название компании
       chatMessage="Привет! Как я могу вам помочь?" // Приветственное сообщение
-      statusMessage="Обычно отвечает быстро"
+      statusMessage="Онлайн"
       avatar="/assets/img/logo/D-logo.svg" // Путь к аватару
       placeholder="Напишите сообщение..."
       allowEsc
